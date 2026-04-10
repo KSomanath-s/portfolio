2ddfe94bb84b9a74ae272ddfbd954262a0257823
Somanath Kakade <somanathkakade5@gmail.com>
2026-04-10 16:11:33 +0530

    fix the text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-        <footer className='bg-gray-900 text-white py-8'>
-            <p className='text-center'>© 2026 SK Web Developer. All rights reserved.</p>
-        </footer>
+        <Footer/>
     </>
   )
 }
